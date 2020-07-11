b0e445a8d258cd4c2e03303948295cb161229f51
gursharan <ggurs096@uottawa.ca>
2020-07-11 02:42:12 -0400

    Trying Constructor

---
+++ codes/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args) {
+        Hell hello = new Hell();
+        hello.hi();
+    }
+}
---
+++ codes/com/ConstructorFirst/ConstVip.java
@@ -0,0 +1,22 @@
+package com.ConstructorFirst;
+public class ConstVip{
+    private String name;
+    private int creditLimit;
+    private String email;
+
+    public ConstVip(){
+        this("Default Name", 600, "Default Email");
+    }
+    public ConstVip(String name, String email){
+        this(name, 600, email);
+    }
+    public ConstVip(String name, int creditLimit, String email){
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public void getStatus(){
+        System.out.println("Name of the person : "+ name +"\nCredit Limit" + creditLimit + "\nEmail Address : " + email);
+    }
+}
