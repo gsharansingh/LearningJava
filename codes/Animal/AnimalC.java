58bf022fe294835676388f80dfdd51fa9f41aaf0
gursharan <ggurs096@uottawa.ca>
2020-07-12 01:29:43 -0400

    Inheritance Completed

---
+++ codes/Animal/AnimalC.java
@@ -0,0 +1,10 @@
+public class AnimalC{
+    private String name;
+    private int size;
+    private int weight;
+
+    public AnimalC(String name, int size, int weight){
+        this.name = name;
+        
+    }
+}
